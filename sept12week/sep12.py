b82e314e369d833cc5814338b636585a30534296
Abby Fischler <46800781+abbyfischler@users.noreply.github.com>
2023-09-14 17:20:10 -0700

    Added blackjack

---
+++ sept12week/sep12.py
@@ -0,0 +1,12 @@
+# constructor it automatically runs when you call the class
+#each object has its own values
+#class robot():
+#def__init__(self, name, color, weight):
+#self.name=name
+#self.color=color
+#class machinegun(self, weapontype):
+#self.weapontype = weapontype
+#robot1=robot("bob",seilver)
+#robot1 is an isntance of the class robot
+# print(robot1.name)
+#difference between class and instance: class is the blueprint and instance is the object
