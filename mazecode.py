0880f771b2ddac2dc6ff2a6413bebc159a000c88
Abby Fischler <46800781+abbyfischler@users.noreply.github.com>
2023-09-27 16:25:08 -0700

    fixed error!

--- mazecode.py
+++ mazecode.py
@@ -33,7 +33,95 @@ class Frontier:
             node = self.frontier[-1]
             self.Frontier = self.frontier[:-1]
             return node
+    def contains_state(self,state):
+        return any (hide.state == state for node in self.frontier)
  
+
+def mazeinit():
+    with open("maze.txt") as f:
+        contents = f.read()
+    if contents.count("A") != 1:
+        raise Exception("A is where the maze starts and there can only be one starting point.")
+    if contents.count("B") != 1:
+        raise Exception("B is the exit of the maze and there is only one exit")
+
+    contents = contents.splitlines()
+    height = len(contents)
+    width = max(len(line) for line in contents)
+    walls = []
+        
+    for i in range(height):
+        row = []
+         #walls (#) are true can't go through them and everything else you can go through. 
+         #eveerything else is falls
+
+        for j in range(width-1):
+            print(i,j)
+            if contents[i][j] == "#":
+                row.append(True)
+                print(width,height)
+            if contents[i][j] == " ":
+                row.append(False)
+            if contents[i][j] == "A":
+                start = (i, j)
+                row.append(False)
+            if contents[i][j] == "B":
+                goal = (i, j)
+                row.append(False)
+                walls.append(row)
+
+    return start, goal, walls
+
+def neighbors(self,state,walls):
+    row,col = state
+    candidates = [["up", (row-1,col)],["down", (row+1,col)],["left"], (row,col-1)], ["right", (row,col+1)]
+    result = []
+    for action, (r,c) in candidates:
+        if walls [r][c] is False:
+            result.append(action,(r,c))
+    return result
+
+def solve(start1,goal):
+    start = Node(state=start1, parent=None, action=None)
+    print(start)
+    frontier=Frontier()
+    explored = set()
+    frontier.add(start)
+    
+    while frontier.empty is False:
+        node = frontier.remove()
+        num_explored += 1
+        if node.state == goal:
+            pass
+
+            if start1 == self.goal:
+                actions = []
+                cells = []
+
+            while node.parent is not None: 
+                actions.append(node.action)
+                cells.append(node.state)
+                node = node.parent
+            actions.reverse()
+            cells.reverse()
+            solution = (actions, cells)
+            return solution
+        explored.add(node.state)
+        for action,state, in neighbors(node.start, walls):
+            if not frontier.contains_state(state) and state not in explored:
+                child = Node(state=state, parent=node, action=action)
+                frontier.add(child)
+        return solutions
+
+start,goal, walls = mazeinit()
+solutions = solve(start, goal)
+
+#call solve
+#
+#come up with a function to solve maze
+    
+
+# make each line a list of the text file
 # read the maze text file in that intalizes it
 # maze.txt is the text file that contains the maze
 
@@ -50,37 +138,3 @@ class Frontier:
 
 #contents[0][3]
 
-def mazeinit():
-
-    with open("maze.txt") as f:
-        contents = f.read()
-        if contents.count("A") != 1:
-            raise Exception("A is where the maze starts and there can only be one starting point.")
-        if contents.count("B") != 1:
-            raise Exception("B is the exit of the maze and there is only one exit")
-
-
-        contents = contents.splitlines()
-        height = len(contents)
-        width = max(len(line) for line in contents)
-        walls = []
-        
-        for i in range(height):
-            row = []
-         #walls are true can't go through them and everything else you can go through. walls are the #
-         #eveerything else is falls
-        
-        for j in width:
-            if contents[i][j] == "#":
-                row.append(True)
-            if contents[i][j] == " ":
-                row.append(False)
-            if contents[i][j] == "A":
-                start = (i, j)
-            if contents[i][j] == "B":
-                goal = (i, j)
-
-
-mazeinit()
-
-# make each line a list of the text file
