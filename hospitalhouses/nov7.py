5c4f8ffb0baaef5358fc824d01a79d6fa0b6a9a3
Abby Fischler <46800781+abbyfischler@users.noreply.github.com>
2023-11-08 11:50:09 -0800

    better

--- hospitalhouses/nov7.py
+++ hospitalhouses/nov7.py
@@ -1,41 +1,110 @@
+def distance(a, b):
+  # x-coordinate difference
+    x_difference = abs(a[0] - b[0])
 
-# #hillclimbiing algorithmn
-# #find local minimum optizamtion
+    # Print the x-coordinate difference
+    print("x-coordinate difference:", x_difference)
 
-#tiny map and calculate the distance
+    #y-coordinate difference
+    y_difference = abs(a[1] - b[1])
 
-import sys
+    # Print y-coordinate difference
+    print("y-coordinate difference:", y_difference)
 
-def mazeinit():
-    with open("hospitalhouses/opti.txt") as f:
-      contents = f.read()
+    #Add the two differences together
+    distance = x_difference + y_difference
 
-    # Create an array to store the coordinates of the B's and H's
-    # B is house
-    # H hotels
-    houseCoordinates = []
-    hospitalCoordinates = []
-    contents = contents.splitlines()
- 
-    # Find the empty spaces, B's, and H's
-    print("height",len(contents))
-    print("width",len(contents[0]))
-    for i in range(len(contents)):
-        for j in range(len(contents[i])):
-           
-            if contents[i][j] == "#":
-                pass
-             # House
-            elif contents[i][j] == "B":
-                print("I j house",i,j)
-                houseCoordinates.append((i, j))
-             # Hospital
-            elif contents[i][j] == "H":
-                hospitalCoordinates.append((i, j))
+    # Print the total difference
+    print("Total difference:", distance)
 
-    print(houseCoordinates,hospitalCoordinates)
+    # Return the total difference
+    return distance
 
-    return houseCoordinates, hospitalCoordinates
+def type(a, b):
+    results = []
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            if a[i][j] == b:
+                results.append((i, j))
+    return results
 
+def getHospital(building, hospitals):
+    distances = []
+    for hospital in hospitals:
+        distances.append((hospital, distance(building, hospital)))
+    return min(distances, key=lambda x: x[1])
 
-mazeinit()
+def getNeighbors(area, pos):
+    y, x = pos
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    neighbors = []
+    for dy, dx in directions:
+        if 0 <= y+dy < len(area) and 0 <= x+dx < len(area[0]):
+            neighbor = (y+dy, x+dx)
+            if area[neighbor[0]][neighbor[1]] != "B":
+                neighbors.append(neighbor)
+    return neighbors
+
+def calcCost(area, oldLoc=None, newLoc=None):
+    hospitals = type(area, "H")
+    buildings = type(area, "B")
+
+    if oldLoc and newLoc:
+        hospitals = [h for h in hospitals if h != oldLoc]
+        hospitals.append(newLoc)
+
+    totalCost = 0
+    for building in buildings:
+        totalCost += distance(building, getHospital(building, hospitals)[0])
+    return totalCost
+
+def location(area, hospital):
+    currentLocation = hospital
+    lowestCost = calcCost(area)
+
+    while True:
+        neighbors = getNeighbors(area, currentLocation)
+        if not neighbors:
+            break
+
+        nextLocation = None
+        for neighbor in neighbors:
+            newCost = calcCost(area, currentLocation, neighbor)
+            if newCost < lowestCost:
+                lowestCost = newCost
+                nextLocation = neighbor
+
+        if nextLocation:
+            currentLocation = nextLocation
+        else:
+            break
+
+    return currentLocation
+
+def main(area):
+    originalCost = calcCost(area)
+    print(f"Original Cost: {originalCost}")
+
+    updatedHospitalLocations = []
+
+    for hospital in type(area, "H"):
+        optimizedLocation = location(area, hospital)
+        if optimizedLocation != hospital:
+            updatedHospitalLocations.append((hospital, optimizedLocation))
+
+    for old, new in updatedHospitalLocations:
+        area[old[0]][old[1]] = "#"
+        area[new[0]][new[1]] = "H"
+
+    finalCost = calcCost(area)
+    print("Optimized hospital locations:")
+    visualString = [''.join(row) for row in area]
+    for line in visualString:
+        print(line)
+
+    print(f"Final Cost: {finalCost}")
+
+if __name__ == "__main__":
+    with open("a.txt") as f:
+        area = [list(line.strip()) for line in f]
+    main(area)
